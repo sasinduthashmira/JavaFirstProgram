a037be321c66f80d47d1a600a367e99e4df35265
Sasindu Thashmira Munasinghe <75942870+sasinduthashmira@users.noreply.github.com>
2021-09-06 01:19:17 +0530

    This is First Upload

---
+++ class Sasindu.java
@@ -0,0 +1,11 @@
+class Sasindu{
+    public static void main (String args[]){
+        extracted();
+    }
+
+    private static void extracted() {
+        System.out.println("HELLO");
+         System.out.println("HELLO");
+         System.out.println("HELLO");
+    }
+}
