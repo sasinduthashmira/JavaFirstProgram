47e9ff67122fd0d9cbccfdc001c66d1b0ea4d5d9
Sasindu Thashmira Munasinghe <75942870+sasinduthashmira@users.noreply.github.com>
2021-09-06 15:23:01 +0530

    Final Update

--- class Sasindu.java
+++ class Sasindu.java
@@ -1,11 +1,20 @@
 class Sasindu{
     public static void main (String args[]){
+        mySpace();
         extracted();
+        mySpace();
     }
 
     private static void extracted() {
-        System.out.println("HELLO");
-         System.out.println("HELLO");
-         System.out.println("HELLO");
+       
+        System.out.println("Hello World!");
+         System.out.println("I'm Sasindu Thashmira ");
+    }
+
+    private static void mySpace() {
+
+        System.out.println(" ");
+        System.out.println("<-------------------------------->");
+        System.out.println(" ");
     }
 }
